ff788c439d2145c45edfa2f6168ae8fb88ca684b
sabbirsun <sabbirsun1920@gmail.com>
2024-01-22 21:29:33 +0600

    click method

--- event.js
+++ event.js
@@ -1,5 +1,6 @@
 /*Very Very Important*/
 
+// Event Click
 // let btn = document.querySelector("#btn");
 // btn.addEventListener("click", function(e){
 //     alert("Button was Clicked");
@@ -8,3 +9,17 @@
 /*function clicked(){
     alert("The Button Was Clicked");
 }*/
+
+// Event Handling 0
+
+let btn = document.querySelector("#clicked");
+btn.onclick = function(){
+    alert("This Message From DOM 0 Label Handler ");
+}
+
+// Event Handling 2
+
+let DOMTwo = document.querySelector("#DOMTwo");
+DOMTwo.addEventListener("click", function(){
+    alert("Dom Two Label Handeler");
+})
