17c0e4541f7a14f8553cadd34d600bd308a9cde3
sabbirsun <sabbirsun1920@gmail.com>
2024-01-22 20:44:08 +0600

    event function

---
+++ event.js
@@ -0,0 +1,10 @@
+/*Very Very Important*/
+
+// let btn = document.querySelector("#btn");
+// btn.addEventListener("click", function(e){
+//     alert("Button was Clicked");
+// })
+// short part
+/*function clicked(){
+    alert("The Button Was Clicked");
+}*/
