e4a21a31622968436a892d6ead420ee7258b3031
sabbirsun <sabbirsun1920@gmail.com>
2024-01-23 19:58:12 +0600

    mouse event

--- event.js
+++ event.js
@@ -24,5 +24,22 @@ DOMTwo.addEventListener("click", function(){
     alert("Dom Two Label Handeler");
 })*/
 
-// Load Event
+// JS Mouse Event
+
+let track = document.querySelector("#track");
+
+function mouseEvent(e){
+    console.log(e.type);
+}
+track.addEventListener("mousedown", mouseEvent);
+track.addEventListener("mouseup", mouseEvent);
+track.addEventListener("click", mouseEvent);
+track.addEventListener("dblclick", mouseEvent);
+// track.addEventListener("mouseover", mouseEvent);
+// track.addEventListener("mouseout", mouseEvent);
+track.addEventListener("mouseenter", mouseEvent);
+track.addEventListener("mouseleave", mouseEvent);
+track.addEventListener("mousemove", mouseEvent);
+
+
 
